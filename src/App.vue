a26ed4977f2e40228c8f977d557fa086fc68a775
骆裕 <mingyuxuezhang@qq.com>
2024-05-08 15:09:48 +0800

    1.1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,109 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <header>新闻头条</header>
+    <nav>
+      <ul>
+        <li
+          :class="{ navinit: isActive == 'topNews' }"
+          @click="changeNav('topNews')"
+        >
+          头条新闻
+        </li>
+        <li
+          :class="{ navinit: isActive == 'typeNews' }"
+          @click="changeNav('typeNews')"
+        >
+          分类新闻
+        </li>
+      </ul>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
+<script setup>
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+
+const isActive = ref("topNews");
+
+const path = location.href.substring(location.href.lastIndexOf("/") + 1);
+isActive.value = path == "" ? "topNews" : path;
+
+function changeNav(param) {
+  isActive.value = param;
+  if (param == "topNews") {
+    router.push("/topNews");
+  } else if (param == "typeNews") {
+    router.push("/typeNews");
+  }
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+/**************** css reset *******************/
+html,
+body,
+div,
+header,
+nav,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+ul,
+li {
+  margin: 0;
+  padding: 0;
+  font-family: "微软雅黑";
+}
+ul {
+  list-style: none;
+}
+a {
+  text-decoration: none;
 }
 
+header {
+  width: 100%;
+  height: 48px;
+  background-color: #e03d3e;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 20px;
+  color: #fff;
+  letter-spacing: 4px;
+}
 nav {
-  padding: 30px;
+  width: 100%;
+  height: 56px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+nav ul {
+  width: 160px;
+  height: 26px;
+  display: flex;
+  justify-content: space-between;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+nav ul li {
+  width: 70px;
+  height: 26px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.navinit {
+  color: #e03d3e;
+  border-bottom: solid 2px #e03d3e;
 }
 </style>
---
+++ src/views/TopNews.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <img src="../assets/logo.png" />
+    <NewsList :data="newsList" v-if="newsList.length"></NewsList>
+  </div>
+</template>
+
+<script setup>
+import NewsList from "../components/NewsList.vue";
+import { ref } from "vue";
+import axios from "axios";
+
+const newsList = ref([]);
+
+function init() {
+  axios
+    .get("/juheNews?type=top&key=655962c80fdaccf03709b567da3bc795")
+    .then(function (response) {
+      newsList.value = response.data.result.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+init();
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+  height: 100px;
+  display: block;
+}
+</style>
